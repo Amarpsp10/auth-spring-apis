5a62025f7998479c9a070e5dd8a9b34d68315a81
Amar Preet Singh <amarpsp10@gmail.com>
2022-01-12 23:45:23 +0530

    Spring Boot Project intialized

---
+++ src/test/java/com/amar/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
